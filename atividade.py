c94c0154128dd7bead5af05670c57bd081a5f116
Lenilson Campos <lenilson.geovane@gmail.com>
2025-04-19 23:19:21 -0300

    👍
    first commit

---
+++ atividade.py
@@ -0,0 +1,17 @@
+'''Questão 01 - Um professor de matemática deseja construir um programa para gerar uma progressão aritmética (PA).
+Para isso, devem ser informados 3 parâmetros de entrada:
+a) Primeiro termo da PA
+b) Quantidade de termos da PA
+c) Razão dessa PA
+Construa um programa para carregar e imprimir uma lista contendo os termos da PA, bem como a soma dos elementos da PA.
+'''
+PA = []
+t1 = input("Informe o 1º termo da PA: ")
+tn = input("Informe a quantidade de termos que terá essa PA: ")
+razao = input("Informe a razão dessa PA: ")
+for i in range(tn):
+    termo = t1 + 
+    PA.append(termo)
+print(PA)    
+
+    
