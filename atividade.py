cedc3768611090fd2e49cbf5be84700c729cc995
Lenilson Campos <lenilson.geovane@gmail.com>
2025-04-27 14:48:52 -0300

    Update atividade.py

--- atividade.py
+++ atividade.py
@@ -1,38 +1,24 @@
-'''Questão 02 - Os professores de educação física estão organizando uma seletiva para montar a equipe de natação. Para isso, eles convocaram os 7 melhores tempos da última competição e marcaram o tempo de cada um dos nadadores, na prova dos 25 metros, estilo nado livre.
-Considerando que não houveram tempos iguais, construa um programa que leia o nome e o tempo(em segundos) de cada atleta e, em seguida, gere o seguinte relatório:
-a) Nadador com o melhor desempenho:
-b) Nadador com o pior desempenho:
-c) Tempo médio dos nadadores:
-Dica: Crie duas listas: uma para cadastrar os nomes dos nadadores e outra para guardar os seus respectivos tempos.
-
+'''Questão 03 - Uma turma de formandos está vendendo rifas para angariar recursos financeiros para sua cerimônia de formatura. Construa um programa para cadastrar os nomes das pessoas que compraram a rifa.  Ao fim, o programa deve sortear o ganhador e imprimir o nome dele.
+Para desenvolver esse programa, observe o exemplo a seguir de como fazer o sorteio em uma lista:
+import random
+lista = [1,2,3,4,5]
+random.shuffle(lista)              #embaralha a lista
+sorteado = random.choice(lista)    #sorteia aleatoriamente um elemento
+print(sorteado)  
 '''
 
-nadadores = []
-tempos = []
-resp = "S"
-soma_tempos = 0
-count = 0
-
+rifa = []
+resp = "s"
 while True:
     if resp.upper() == "S":
-        nome = input("Informe o NOME do nadador: ")
-        nadadores.append(nome)
-        time = int(input("Informe o TEMPO marcado: "))
-        soma_tempos +=time
-        count += 1
-        tempos.append(time)
-        resp = input("Incluir dados de outro NADADOR: [S|N]: ")
+        nome = input("Informe um nome: ")
+        rifa.append(nome)
+        resp = input("Gostaria de cadastrar mais um nome[S|N] ?")  
     else:
-        break    
-menor_tempo = tempos.index(min(tempos))
-print(nadadores)
-print(tempos)
-melhor_nadador = nadadores[menor_tempo]
-melhor_tempo = tempos[menor_tempo]
-maior_tempo = tempos.index(max(tempos))
-pior_nadador = nadadores[maior_tempo]
-pior_tempo = tempos[maior_tempo]
-print("Nadador com melhor desempenho: {}, com o tempo de {} segundos".format(melhor_nadador,melhor_tempo))
-print("Nadador com o pior desempenho: {}, com o tempo de {} segundos".format(pior_nadador,pior_tempo))
-media_tempos = soma_tempos/count
-print("A média dos tempos dos nadadores é de: {} segundos".format(media_tempos))
+        break
+print("Os participantes do sorteio são:")
+print(rifa)
+import random
+random.shuffle(rifa)
+sorteado = random.choice(rifa)
+print("O ganhador foi: {}".format(sorteado))
